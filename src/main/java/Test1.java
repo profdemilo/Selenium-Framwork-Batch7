805124e55dcd53b3bf86d71ab82e33074a2343a5
Emile Esapa <emile.esapa@the-mcorp.com>
2024-09-20 13:03:07 -0500

    assingment for orangecrm given

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test1 {
     public static void main(String[] args){
 
-        System.out.println("Hello, World!");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
     }
 }
