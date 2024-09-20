8ac67264cd76bf7e0b0dee5d8f33a69eb2bbbd19
Emile Esapa <emile.esapa@the-mcorp.com>
2024-09-20 13:12:13 -0500

    refactor

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
     public static void main(String[] args){
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
     }
